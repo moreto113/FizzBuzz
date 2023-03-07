bd87fc31cbd2b2f57a1916a62853d2cadb164331
oretom518 <oretom518>
2023-03-07 15:01:11 -0600

    FizzBuzz now works

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,1 +1,12 @@
-print("Hello world");
+for i in range(1,101):
+    if i%3==0:
+        print("Fizz")
+    
+    if i%5==0:
+        print("Buzz")
+    if i%3==0 & i%5==0:
+        print("FizzBuzz")
+    else :
+        print(i)
+
+
